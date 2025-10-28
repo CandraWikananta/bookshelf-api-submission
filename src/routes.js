8907145ad049cefbd9505169ddf110782ac50e5f
Candra Wikananta <candrawikananta@gmail.com>
2025-10-28 18:13:16 +0800

    added: addBookHandler untuk menambahkan buku, dan validasi jika buku yang ditambahkan tidak ada nama dan readpage > pagecount

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,36 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+  deleteBookByIdHandler,
+} = require('./handler');
+
 const routes = [
   {
     method: 'POST',
     path: '/books',
-    handler: () => {},
+    handler: addBookHandler,
   },
   {
     method: 'GET',
     path: '/books',
-    handler: () => {},
+    handler: getAllBooksHandler,
   },
   // {
   //   method: 'GET',
   //   path: '/books/{id}',
-  //   handler: () => {},
+  //   handler: getBookByIdHandler,
   // },
   // {
   //   method: 'PUT',
   //   path: '/books/{id}',
-  //   handler: () => {},
+  //   handler: editBookByIdHandler,
   // },
   // {
   //   method: 'DELETE',
   //   path: 'books/{id}',
-  //   handler: () => {},
+  //   handler: deleteBookByIdHandler,
   // }
 ];
 
